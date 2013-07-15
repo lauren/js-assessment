e105d896964912b7f141b8a7cbcd933853abfe32
Lauren Sperber <lauren.sperber@gmail.com>
2013-07-15 11:06:08 -0400

    added code for module tests

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,14 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+    	var Constructor = function (str1, str2) {
+    		this.name = str2;
+    		this.greeting = str1;
+    		this.sayIt = function () {
+    			return this.greeting + ", " + this.name;
+    		}
+    	}
+    	return new Constructor(str1, str2);
     }
   };
 });
