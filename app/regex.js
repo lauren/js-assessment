6f6b1664b54c6e6fc13e49a47195ebe5361462b9
Lauren Sperber <lauren.sperber@gmail.com>
2013-07-16 15:54:57 -0400

    got regex tests to pass

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,31 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return /\d/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return /([A-Za-z])\1+/.test(str)
     },
 
     endsWithVowel : function(str) {
-
+      return /[aeiou]$/i.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+      if (/\d{3}/.test(str)) {
+        return str.match(/\d{3}/)[0];
+      } else {
+        return false;
+      }
     },
 
     matchesPattern : function(str) {
-
+      return /^\d{3}-\d{3}-\d{4}$/.test(str);
     },
-    isUSD : function(str) {
 
+    isUSD : function(str) {
+      return /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/.test(str);
     }
   };
 });
